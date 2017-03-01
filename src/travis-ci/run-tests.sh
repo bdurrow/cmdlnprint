a38b2d216c607b7b24ea3b2356cd2ab831171d47
Brad Durrow <brad@nolab.org>
2017-03-01 11:33:26 -0700

    xvfb improvements and failure screenshot

--- src/travis-ci/run-tests.sh
+++ src/travis-ci/run-tests.sh
@@ -2,6 +2,11 @@
 set -euo pipefail
 IFS=$'\n\t'
 
+DISPLAY=${DISPLAY-}
+FB_DIR=${FB_DIR-/tmp/firefox-fb}
+FB_RESOLUTION=${FB_RESOLUTION-1024x768}
+FB_PIXEL_DEPTH=${FB_PIXEL_DEPTH-24}
+
 cd ${TRAVIS_BUILD_DIR}
 bash src/travis-ci/fetch-firefox.sh release-linux64-add-on-devel
 FIREFOX_INSTALL_DIR=${TRAVIS_BUILD_DIR}/firefox/
@@ -22,6 +27,21 @@ set -x
 echo "Modifying firefox defaults to suit our needs into ${FIREFOX_INSTALL_DIR}..."
 #tar --directory ${TRAVIS_BUILD_DIR}/docker/root/usr/lib/firefox/ cf - ./ | tar --directory ${FIREFOX_INSTALL_DIR} xf -
 rsync -av ${TRAVIS_BUILD_DIR}/docker/root/usr/lib/firefox/ ${FIREFOX_INSTALL_DIR}/
+
+
+if [[ -n ${DISPLAY} ]]; then
+  echo "DISPLAY already set to ${DISPLAY}.  Skipping Xvfb setup."
+else
+  if ! [[ -e ${FB_DIR} ]]; then
+    echo "Making ${FB_DIR}..."
+    mkdir -p ${FB_DIR}
+  fi
+  echo "Setting up Xvfb in ${FB_DIR}..."
+
+  Xvfb :1 -screen 0 ${FB_RESOLUTION}x${FB_PIXEL_DEPTH} -fbdir ${FB_DIR} 2>&1 | \
+    egrep -v '^(_XSERVTransmkdir.*will not be created\.|Xlib: extension "RANDR" missing on display .*\.)$' &
+  export DISPLAY=":1"
+fi
 
 sh -e /etc/init.d/xvfb start
 ${FIREFOX_INSTALL_DIR}/firefox -foreground -no-remote -print https://google.com -print-mode pdf -print-file /tmp/test1.pdf
